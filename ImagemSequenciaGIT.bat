ad79a929f52aca9bd115d2f06bc2a2a0478a563d
BI-CIPP <complexo.bi@complexodopecem.com.br>
2026-03-30 11:47:09 -0300

    Upload SEQUÊNCIA DE ATRACAÇÃO.png

--- ImagemSequenciaGIT.bat
+++ ImagemSequenciaGIT.bat
@@ -4,7 +4,7 @@ REM Rodar Python invisível (pythonw.exe) via Power Automate
 REM ==================================================
 
 SET PYTHONW_EXE="C:\Users\francisco.horizonte\AppData\Local\Programs\Python\Python312\python.exe"
-SET SCRIPT="C:\OneDrive\OneDrive - CIPP\SEQUENCIA POWER BI\ImagemSequenciaGit.py"
+SET SCRIPT="https://github.com/BI-CIPP/ImagemSequencia.git"
 
 REM Executa o Python invisível
 %PYTHONW_EXE% %SCRIPT% %INPUT_DIR% %OUTPUT_DIR%
